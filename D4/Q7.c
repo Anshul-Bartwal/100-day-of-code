6b443193ee0ad4d7456575e046253e4eefebfc24
Anshul Bartwal <anshulbartwal720@gmail.com>
2025-08-29 21:37:02 +0530

    Added Day 4

---
+++ D4/Q7.c
@@ -0,0 +1,8 @@
+//Write a program to swap two numbers without using a third variable.
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("After swap: %d %d ",b,a);
+    return 0;
+}
