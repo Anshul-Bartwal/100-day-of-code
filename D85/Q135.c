943969b4eef131c097db2f0042f9b74ea0b696e6
Anshul Bartwal <anshulbartwal720@gmail.com>
2025-11-26 10:11:27 +0530

    Day 85

---
+++ D85/Q135.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef enum {
+    A = 10,
+    B,
+    C
+} Letters;
+
+int main() {
+
+    printf("A=%d\n", A);
+    printf("B=%d\n", B);
+    printf("C=%d\n", C);
+
+    return 0;
+}
